b6cc03e37b39d134298272058e3498c90a051f6e
arthurmaciel <arthurmaciel@gmail.com>
2013-12-17 12:50:04 -0200

    Updated initial tests - still need improvements)

--- trunk/tests/run.scm
+++ trunk/tests/run.scm
@@ -8,23 +8,7 @@
 (define responses (process-request ship-req))
 
 (for-each (lambda (r)
-	    (test-assert "check if object is correios response"
-			 (response? r)))
+	    (test-assert "check if object is correios response" (response? r)))
 	  responses)
-
-;; (let loop ((responses (process-request ship-req)))
-;;   (cond ((null? responses) 
-;; 	 (printf "Finished processing responses.~N"))
-;; 	(else
-;; 	 (let ((response (car responses)))
-;; 	   (if (valid-response? response)
-;; 	       (printf "Service ~A - Cost: ~A   Estimated delivery (days): ~A~N" 
-;; 		 (response-service response)
-;; 		 (response-cost response)
-;; 		 (response-delivery-time response))
-;; 	       (printf "Service ~A - Error:  ~A" 
-;; 		 (response-service response)
-;; 		 (response-error-msg response))))
-;; 	 (loop (cdr responses)))))
 
 (test-exit)
